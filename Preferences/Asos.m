4c584600dd33b7bfb4b9beddc7056bdf0d6233c8
Phillip Tennen <phillip.ennen@gmail.com>
2014-11-13 15:28:14 -0700

    Add twitter button

--- Preferences/Asos.m
+++ Preferences/Asos.m
@@ -91,7 +91,21 @@ void prefsTouchUnlock() {
 @implementation AsosListController
 
 -(void)openTwitter {
-
+	NSString *user = @"phillipten";
+	if([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tweetbot:"]])
+		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[@"tweetbot:///user_profile/" stringByAppendingString:user]]];
+	
+	else if([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"twitterrific:"]])
+		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[@"twitterrific:///profile?screen_name=" stringByAppendingString:user]]];
+	
+	else if([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"tweetings:"]])
+		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[@"tweetings:///user?screen_name=" stringByAppendingString:user]]];
+	
+	else if([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"twitter:"]])
+		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[@"twitter://user?screen_name=" stringByAppendingString:user]]];
+	
+	else
+		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:[@"https://mobile.twitter.com/" stringByAppendingString:user]]];
 }
 
 - (id)initForContentSize:(CGSize)size {
